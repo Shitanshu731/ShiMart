91d3bfb84f75e438ed88133bb09a17ea1a1b0b41
Shitcoder731 <shitanshu.731@gmail.com>
2025-02-15 01:04:49 +0530

    Mongo Db

--- src/lib/db.js
+++ src/lib/db.js
@@ -3,31 +3,16 @@ import mongoose from "mongoose";
 const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
-  throw new Error("Please define MONGODB_URI in your environment variables");
+  throw new Error("Please define the MONGODB_URI environment variable");
 }
 
-let cached = global.mongoose || { conn: null, promise: null };
+const connectDB = async () => {
+  if (mongoose.connections[0].readyState) return;
+  await mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log("MongoDB Connected");
+};
 
-export default async function connectDB() {
-  if (cached.conn) {
-    return cached.conn;
-  }
-
-  if (!cached.promise) {
-    cached.promise = mongoose
-      .connect(MONGODB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      })
-      .then((mongoose) => mongoose);
-  }
-
-  try {
-    cached.conn = await cached.promise;
-    return cached.conn;
-  } catch (error) {
-    cached.promise = null;
-    console.error("Database connection error:", error);
-    throw error;
-  }
-}
+export default connectDB;
